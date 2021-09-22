4d551e40b41f6ae0f7401d09edd0885164d07357
Senthil <rsenthil75@hotmail.com>
2021-09-21 19:37:04 -0500

    Adding Test

---
+++ Tests/src/test/java/com/practice/Test1.java
@@ -0,0 +1,13 @@
+package com.practice;
+
+import com.example.Pages;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Test1 {
+    @Test
+    public void test1() {
+
+
+    }
+}
